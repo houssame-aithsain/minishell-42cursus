94b65e707b458b93421902be644e08de64cc1d2e
Houssame Ait hsain <hossame@inbox.ru>
2023-04-13 20:51:55 +0000

    Update readl_and_exec.c

--- readl_and_exec.c
+++ readl_and_exec.c
@@ -6,32 +6,85 @@
 /*   By: hait-hsa <hait-hsa@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/01 21:33:53 by hait-hsa          #+#    #+#             */
-/*   Updated: 2023/04/12 22:03:49 by hait-hsa         ###   ########.fr       */
+/*   Updated: 2023/04/13 20:51:41 by hait-hsa         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-void readl_and_exec()
+int	ft_checker(char *line, int count, int cout)
+{
+	char x;
+
+	
+	while(line && line[count])
+	{
+		if (line[count++] == '"' || line[count++] == 39)
+			return 1;
+	}
+	return 0;
+}
+
+char	*ft_parse(char *line)
+{
+	int		cout;
+	int		check;
+	int		count;
+	int		j;
+	int		i;
+	int		index;
+	char	*arr;
+
+	i = 0;
+	j = 0;
+	index = 0;
+	check = 0;
+	arr = malloc(ft_strlen(line) + 1);
+	while(line && line[i])
+	{
+		if (line[i] && (line[i] == '"' || line[i] == 39))
+		{
+			if (line[i] == '"')
+				cout = 1;
+			else
+				cout = 2;
+			count = i;
+			count++;
+			check = ft_checker(line, count, cout);
+			while(line[count] && check && (line[count] != '"' || line[count] != 39))
+			{
+				if (line[count] == '"' || line[count] == 39)
+					break;
+				if (line[count] == ' ')
+					line[count] = '^';
+				// printf("line:=> |%c|",line[count]);
+				count++;
+			}
+			i++;
+		}
+		else
+		{
+			arr[j++] = line[i++];
+			// printf("->|%c|<-",arr[j++]);
+		}
+	}
+	arr[j] = 0;
+	return arr;
+}
+
+void readl_and_parse()
 {
 	int i;
 	char *line;
-	char **ncoom;
-	char env[14] = "PATH=/usr/bin\0";
-	char *envp;
+	char *ncoom;
 
 	ncoom = NULL;
-	envp = env;
 	while (TRUE)
 	{
 		i = 0;
 		line = NULL;
 		line = readline("minishell$> ");
-		ncoom = ft_split(line, ' ');
-		while (ncoom && ncoom[i])
-			printf("%s ", ncoom[i++]);
-		printf("\n");
-		free(ncoom);
-		free(line);
+		ncoom = ft_parse(line);
+		printf("str: %s\n",ncoom);
 	}
 }
