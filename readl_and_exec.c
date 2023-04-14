1432dfd027404f4f429fdfadb49f9b0fb1732a6d
Houssame Ait hsain <hossame@inbox.ru>
2023-04-14 01:54:18 +0000

    update101

--- readl_and_exec.c
+++ readl_and_exec.c
@@ -6,11 +6,25 @@
 /*   By: hait-hsa <hait-hsa@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/01 21:33:53 by hait-hsa          #+#    #+#             */
-/*   Updated: 2023/04/13 21:07:36 by hait-hsa         ###   ########.fr       */
+/*   Updated: 2023/04/14 01:48:06 by hait-hsa         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
+
+int	ft_grep(char *line, int i)
+{
+	while(line[i])
+	{
+		if (line[i] == '"')
+			return 1;
+		else if (line[i] == 39)
+			return 2;
+		else if (line[i] == ' ')
+			return 3;
+	}
+	return 0;
+}
 
 int	ft_checker(char *line, int count, int cout)
 {
@@ -30,6 +44,8 @@ int	ft_checker(char *line, int count, int cout)
 
 char	*ft_parse(char *line)
 {
+	int		pipe;
+	int		copy;
 	int		valid;
 	int		cout;
 	int		check;
@@ -38,16 +54,23 @@ char	*ft_parse(char *line)
 	int		i;
 	int		index;
 	char	*arr;
+	char	swit;
 
 	i = 0;
 	j = 0;
 	index = 0;
 	check = 0;
 	valid = 0;
+	copy = 0;
+	pipe = 0;
 	arr = malloc(ft_strlen(line) + 1);
 	while(line && line[i])
 	{
-		if (line[i] && (line[i] == '"' || line[i] == 39))
+		if (!pipe)
+			if(line[i] && line[i] == '|' && (((line[i + 1] && line[i + 1] == 39) || (line[i - 1] && line[i - 1] == 39))
+				|| ((line[i + 1] && line[i + 1] == '"') || (line[i - 1] && line[i - 1] == '"'))))
+				arr[j++] = '^';
+		if (line[i] && (line[i] == '"' || line[i] == 39) && (ft_memcmp(line + i, "echo", 4 || ft_memcmp(line + i, "ECHO", 4))))
 		{
 			if (line[i] == '"')
 				cout = 1;
@@ -69,7 +92,30 @@ char	*ft_parse(char *line)
 		}
 		else
 		{
-			arr[j++] = line[i++];
+			if (!ft_memcmp(line + i, "echo", 4) || !ft_memcmp(line + i, "ECHO", 4))
+			{
+				while(line[i] && line[i] != '|')
+					arr[j++] = line[i++];
+			}
+			else if (!ft_memcmp(line + i, "grep", 4) || !ft_memcmp(line + i, "GREP", 4))
+			{
+				while(line[i] && copy < 4)
+				{
+					arr[j++] = line[i++];
+					copy++;
+				}
+				if ((ft_grep(line, i)) == 1)
+					swit = '"';
+				else if ((ft_grep(line, i)) == 2)
+					swit = 39;
+				else if ((ft_grep(line, i)) == 3)
+					swit = ' ';
+				arr[j++] = line[i++];
+				while(line[i] && line[i] != swit)
+					arr[j++] = line[i++];
+			}
+			else
+				arr[j++] = line[i++];
 		}
 	}
 	arr[j] = 0;
