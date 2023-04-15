d0222b62bdfb7ca77feb7c6b6a9c568b8394be7a
Houssame Ait hsain <hossame@inbox.ru>
2023-04-15 21:31:29 +0000

    update999

--- minishell.h
+++ minishell.h
@@ -8,7 +8,28 @@
 #include "./libft/libft.h"
 
 #define TRUE 1
+#define ECHO 2
+#define GREP 1
+#define ECHO_CHECK 0
 
-void	readl_and_exec();
 
+typedef struct var
+{
+	int		j;
+	int		i;
+	int		index;
+	int		pipe_count;
+	int		copy_count;
+	int		valid_quote;
+	int		quote_type;
+	int		if_quote;
+	int		quote_count;
+	char	*parsed_arr;
+	char	quote_holder;
+} t_var;
+
+void	readl_to_parse();
+int		if_grep(char *line, int i, int flag);
+char	*parse_input(char *line, t_var *var);
+int		ft_loop(char *line, t_var *var);
 #endif
